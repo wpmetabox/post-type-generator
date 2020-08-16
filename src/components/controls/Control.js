a1ce7afda1b9dce7466b23765cff006817b986eb
Thomas <latiosthinh@live.com>
2020-08-16 20:31:45 +0700

    refractor control.js

--- src/components/controls/Control.js
+++ src/components/controls/Control.js
@@ -8,36 +8,31 @@ import Radio from './Radio';
 const Control = ( {props} ) => {
 	const [state, setState] = useContext( PhpSettings );
 
-	const updateText = e => {
+	const handleUpdate = e => {
 		const name = e.target.name;
-		const value = e.target.value;
+		let value;
 
-		setState( state => ( {...state, [name]: value} ) )
-	}
-
-	const updateCheck = e => {
-		const name = e.target.name;
-		const value = e.target.checked;
-
-		setState( state => ( {...state, [name]: value} ) )
-	}
-
-	const updateRadio = e => {
-		const name = e.target.name;
-		const value = e.target.value;
+		switch ( e.target.type ) {
+			case 'checkbox':
+				value = e.target.checked;
+				break;
+			default:
+				value = e.target.value;
+				break;
+		}
 
 		setState( state => ( {...state, [name]: value} ) )
 	}
 
 	switch (props.type) {
 		case 'text':
-			return <Input label={props.label} name={props.name} placeholder={props.placeholder} required={props.required} update={updateText} />
+			return <Input label={props.label} name={props.name} placeholder={props.placeholder} required={props.required} update={handleUpdate} />
 		case 'textarea':
-			return <Textarea label={props.label} name={props.name} placeholder={props.placeholder} update={updateText} />
+			return <Textarea label={props.label} name={props.name} placeholder={props.placeholder} update={handleUpdate} />
 		case 'checkbox':
-			return <Checkbox label={props.label} name={props.name} description={props.description} update={updateCheck} checked={props.checked} />
+			return <Checkbox label={props.label} name={props.name} description={props.description} update={handleUpdate} checked={props.checked} />
 		case 'radio':
-			return <Radio label={props.label} name={props.name} values={props.values} defaultValue={props.defaultValue} update={updateRadio} />
+			return <Radio label={props.label} name={props.name} values={props.values} defaultValue={props.defaultValue} update={handleUpdate} />
 		default:
 			break;
 	}
--- src/components/controls/Radio.js
+++ src/components/controls/Radio.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// 
 
 const Radio = ( {label, name, update, values, defaultValue} ) => {
 	return (
